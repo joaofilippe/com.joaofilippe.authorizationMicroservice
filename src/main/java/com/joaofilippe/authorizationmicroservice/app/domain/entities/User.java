60a69d463207cf6e22292935fc39f7a301ebd869
João Filippe Rossi Rodrigues <joaofilippe@outlook.com>
2024-12-29 22:38:19 -0300

    initial commit

--- src/main/java/com/joaofilippe/authorizationmicroservice/app/domain/entities/User.java
+++ src/main/java/com/joaofilippe/authorizationmicroservice/app/domain/entities/User.java
@@ -2,13 +2,24 @@ package com.joaofilippe.authorizationmicroservice.app.domain.entities;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
     private UUID id;
     private String username;
     private String password;
     private String email;
+
+    public User(@NonNull String username, @NonNull String password, @NonNull String email) {
+        this.id = UUID.randomUUID();
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
 }
---
+++ src/main/java/com/joaofilippe/authorizationmicroservice/app/domain/usecases/user/CreateUseCase.java
@@ -0,0 +1,7 @@
+package com.joaofilippe.authorizationmicroservice.app.domain.usecases.user;
+
+public class CreateUseCase {
+    public void execute() {
+
+    }
+}
