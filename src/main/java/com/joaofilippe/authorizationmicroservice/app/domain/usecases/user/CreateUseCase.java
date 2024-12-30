617c02296afe11558edce7c53a7b8ae2068009dc
João Filippe Rossi Rodrigues <joaofilippe@outlook.com>
2024-12-30 20:33:01 -0300

    feat: insert create use case

--- src/main/java/com/joaofilippe/authorizationmicroservice/app/domain/usecases/user/CreateUseCase.java
+++ src/main/java/com/joaofilippe/authorizationmicroservice/app/domain/usecases/user/CreateUseCase.java
@@ -1,7 +1,20 @@
 package com.joaofilippe.authorizationmicroservice.app.domain.usecases.user;
 
-public class CreateUseCase {
-    public void execute() {
+import com.joaofilippe.authorizationmicroservice.app.domain.entities.User;
+import com.joaofilippe.authorizationmicroservice.app.domain.repositories.IUseRepository;
 
+public class CreateUseCase {
+    private final IUseRepository userRepository;
+
+    public CreateUseCase(IUseRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void execute(User user) {
+        if (user == null) {
+            throw new IllegalArgumentException("User cannot be null");
+        }
+
+        userRepository.save(user);
     }
 }
