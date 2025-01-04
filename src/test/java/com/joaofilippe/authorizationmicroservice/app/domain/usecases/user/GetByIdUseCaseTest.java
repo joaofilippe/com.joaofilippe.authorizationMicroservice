e014cbd730852686e6365a2f41f3a47eadff308c
João Filippe Rossi Rodrigues <joaofilippe@outlook.com>
2025-01-04 09:56:38 -0300

    i really don't know

--- src/test/java/com/joaofilippe/authorizationmicroservice/app/domain/usecases/user/GetByIdUseCaseTest.java
+++ src/test/java/com/joaofilippe/authorizationmicroservice/app/domain/usecases/user/GetByIdUseCaseTest.java
@@ -1,7 +1,26 @@
 package com.joaofilippe.authorizationmicroservice.app.domain.usecases.user;
 
+import com.joaofilippe.authorizationmicroservice.app.domain.repositories.IUserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GetByIdUseCaseTest {
-    final IUserRepository
+    private IUserRepository userRepository;
+    private GetByIdUseCase getByIdUseCase;
+
+    @BeforeEach
+    void setUp(){
+        userRepository = Mockito.mock(IUserRepository.class);
+        getByIdUseCase = new GetByIdUseCase(userRepository);
+    }
+
+    @Test
+    @DisplayName("Should throw IllegalArgumentException when id is null")
+    public void execute_ShouldThrowIllegalArgumentException_WhenIdIsNull(){
+        assertThrows(IllegalArgumentException.class, () -> getByIdUseCase.execute(null));
+    }
 }
